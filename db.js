89776121f3ee251d16c921d0afa180dee5e9bfbb
Phoenix Nemo <i@phoenixlzx.com>
2021-08-01 14:31:22 +0900

    prepare for pages

--- db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const config = require("./config/db_config.json").mysql;
-const initModels = require("./models/init-models");
-
-const sequelize = new Sequelize(config.database, config.user,config.password,config);
-let db = initModels(sequelize);
-db.sequelize = sequelize;
-
-module.exports = db;
